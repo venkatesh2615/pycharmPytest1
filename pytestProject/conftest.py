c06b5fe007f7395e85584aceb0e4c32e5ecf6802
venkatesan <venkatesh2615@gmail.com>
2021-09-16 11:05:24 +0530

    conftest development commit 1

--- pytestProject/conftest.py
+++ pytestProject/conftest.py
@@ -7,3 +7,8 @@ def response():
     res = requests.get("https://www.google.com")
     print("Hello")
     return res
+
+
+@pytest.fixture(params=[(3, 3, 6), (7, 7, 14)])
+def passParam(request):
+    return request.param
