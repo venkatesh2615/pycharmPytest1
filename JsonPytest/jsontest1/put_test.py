35e98e1c32b5b55c3dbdb15abb140feb6423b861
venkatesan <venkatesh2615@gmail.com>
2021-09-21 11:22:08 +0530

    put file updated 1

--- JsonPytest/jsontest1/put_test.py
+++ JsonPytest/jsontest1/put_test.py
@@ -5,9 +5,5 @@ import requests
 
 def test_put_Res(response_put):
     put_data = json.loads(response_put.content)
-    print(put_data)
     assert response_put.status_code == 200
-
     res = requests.get("https://reqres.in/api/users/2")
-    print(res.url)
-    print(res.content)
