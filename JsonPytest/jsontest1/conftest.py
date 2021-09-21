dc65c7a530d1052e4807cbc5454190a3a7085954
venkatesan <venkatesh2615@gmail.com>
2021-09-21 11:21:07 +0530

    conftest updated 1

--- JsonPytest/jsontest1/conftest.py
+++ JsonPytest/jsontest1/conftest.py
@@ -17,3 +17,7 @@ def response_post(request):
     name = request.param[0]
     job = request.param[1]
     return requests.post(f"{base_url}api/users", data={"name": {name}, "job": {job}})
+
+@pytest.fixture()
+def response_put():
+    return requests.put(f"{base_url}api/user/2", data={"name":"venkatesan","job":"python Developer"})
